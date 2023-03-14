f5ca898f9f83efc91a118c5920f393bd79d76c2f
Timothy Ingledue <timothy.ingledue@orion.com>
2023-03-14 15:31:58 -0500

    Set up weather collection view.

--- MauiWeatherApp/WeatherApp/WeatherPage.xaml.cs
+++ MauiWeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -5,15 +5,26 @@ namespace WeatherApp;
 
 public partial class WeatherPage : ContentPage
 {
+    public List<Models.List> WeatherList { get; set; }
+
 	public WeatherPage()
 	{
 		InitializeComponent();
+        WeatherList = new List<Models.List>();
 	}
 
     protected override async void OnAppearing()
     {
 		base.OnAppearing();
         var result = await ApiService.GetWeather(41.140453, -96.243683);
+
+        foreach (var item in result.List)
+        {
+            WeatherList.Add(item);
+        }
+
+        CvWeather.ItemsSource = WeatherList;
+
         lblCity.Text = result.City.Name;
         lblWeatherDescription.Text = result.List[0].Weather[0].Description;
         lblTemperature.Text = $"{result.List[0].Main.Temperature}{GlobalConstants.TemperatureFahrenheit}";
